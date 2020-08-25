fe7348f153a2b290d5e57b38fb65e81e43148cba
tikken <tikken@tikkens-MacBook-Air.local>
2020-08-25 16:53:14 +0300

    renders another appointment when selected

--- assets/js/components/Appointment.js
+++ assets/js/components/Appointment.js
@@ -1,5 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react';
 
-export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
+export const Appointment = ({customer}) => <div>{customer.firstName}</div>;
 
-export const AppointmentsDayView = () => null;
+export const AppointmentsDayView = ({appointments}) => {
+    const [selectedAppointment, setSelectedAppointment] = useState(
+        0
+    );
+    return (
+        <div id="appointmentsDayView">
+            <ol>
+                {appointments.map((appointment,i) => (
+                    <li key={appointment.startsAt}>
+                        <button type="button" onClick={() => setSelectedAppointment(i)}>
+                            {appointment.startsAt}
+                        </button>
+                    </li>))}
+
+                {appointments.length === 0 ? (
+                    <p>There are no appointments scheduled for today.</p>
+                ) : (
+                    <Appointment {...appointments[selectedAppointment]} />
+                )}
+
+            </ol>
+        </div>
+    );
+};
--- assets/tests/Appointment.test.js
+++ assets/tests/Appointment.test.js
@@ -4,6 +4,7 @@ import {
     Appointment,
     AppointmentsDayView
 } from "../js/components/Appointment";
+import ReactTestUtils from 'react-dom/test-utils';
 
 describe('Appointment', () => {
     let container;
@@ -14,6 +15,18 @@ describe('Appointment', () => {
     });
 
     const render = (component) => ReactDOM.render(component, container);
+
+    const today = new Date();
+    const appointments = [
+        {
+            startsAt: today.setHours(12, 0),
+            customer: { firstName: 'Ashley' }
+        },
+        {
+            startsAt: today.setHours(13, 0),
+            customer: { firstName: 'Jordan' }
+        }
+    ];
 
     it('renders the customer first name', () => {
        customer = { firstName: 'Ashley' };
@@ -30,6 +43,38 @@ describe('Appointment', () => {
     it('renders a div with the right id', () => {
         render(<AppointmentsDayView appointments={[]} />);
         expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    });
+
+    it('renders multiple appointments in an ol element', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelector('ol')).not.toBeNull();
+        expect(
+            container.querySelector('ol').children
+        ).toHaveLength(3);
+    });
+
+    it('initially shows a message saying there are no appointments today', () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        expect(container.textContent).toMatch(
+            'There are no appointments scheduled for today.'
+        );
+    });
+
+    it('has a button element in each li', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(
+            container.querySelectorAll('li > button')
+        ).toHaveLength(2);
+        expect(
+            container.querySelectorAll('li > button')[0].type
+        ).toEqual('button');
+    });
+
+    it('renders another appointment when selected', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        const button = container.querySelectorAll('button')[1];
+        ReactTestUtils.Simulate.click(button);
+        expect(container.textContent).toMatch('Jordan');
     });
 });
 
